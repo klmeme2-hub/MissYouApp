fa3a4b030fdd8a73e21d68b666b0c8fe9017b3bf
klmeme2-hub <klmeme2@gmail.com>
2025-12-11 21:11:48 +0800

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,1 +1,12 @@
+# modules/config.py
 
+# 角色中英對照表 (解決中文檔名問題)
+ROLE_MAPPING = {
+    "妻子": "wife",
+    "丈夫": "husband",
+    "兒子": "son",
+    "女兒": "daughter",
+    "朋友": "friend",
+    "孫子": "grandson",
+    "其他": "others"
+}
